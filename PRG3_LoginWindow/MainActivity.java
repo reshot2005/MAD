786a6f526bc03379b3ab09ff82f90343f442d314
reshot2005 <reshotofficial01@gmail.com>
2026-06-12 01:10:28 +0530

    Update PRG3 MainActivity

---
+++ PRG3_LoginWindow/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.loginapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // UI elements are defined in XML
+        // No additional functionality in this program
+    }
+}
